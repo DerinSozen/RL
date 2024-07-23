5bc993e3d338788d268eaa10d4bf0e22b84712ad
DerinSozen <67414435+DerinSozen@users.noreply.github.com>
2024-07-23 13:34:16 +0300

    Github README

--- PPO/PPO.py
+++ PPO/PPO.py
@@ -3,8 +3,81 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
+from torch.distributions import Categorical
 import gymnasium as gym
 
-class PPO_Agent:
+class Buffer:
     def __init__(self):
+        self.actions = []
+        self.states = []
+        self.logprobs = []
+        self.rewards = []
+        self.state_values = []
+        self.is_terminals = []
         
+    def clear(self):
+        self.actions = []
+        self.states = []
+        self.logprobs = []
+        self.rewards = []
+        self.state_values = []
+        self.is_terminals = []
+        
+    class Actor(nn.Module):
+        def __init__(self, state_dim, action_dim):
+            super(Actor, self).__init__()
+            self.actor = nn.Sequential(
+                            nn.Linear(state_dim, 64),
+                            nn.Tanh(),
+                            nn.Linear(64, 64),
+                            nn.Tanh(),
+                            nn.Linear(64, action_dim),
+                            nn.Softmax(dim=-1)
+            )
+            
+            self.critic = nn.Sequential(
+                        nn.Linear(state_dim, 64),
+                        nn.Tanh(),
+                        nn.Linear(64, 64),
+                        nn.Tanh(),
+                        nn.Linear(64, 1)
+            )
+            
+        def act(self, state):
+            action_probs = self.actor(state)
+            dist = Categorical(action_probs)
+
+            action = dist.sample()
+            action_logprob = dist.log_prob(action)
+            state_val = self.critic(state)
+
+            return action.detach(), action_logprob.detach(), state_val.detach()
+        
+        def evaluate(self, state, action):
+            action_probs = self.actor(state)
+            dist = Categorical(action_probs)
+            action_logprobs = dist.log_prob(action)
+            dist_entropy = dist.entropy()
+            state_values = self.critic(state)
+            
+            return action_logprobs, state_values, dist_entropy
+        
+class PPO:
+    def __init__(self, state_dim, action_dim, lr_actor, lr_critic, gamma, K_epochs, eps_clip):
+
+        self.gamma = gamma
+        self.eps_clip = eps_clip
+        self.K_epochs = K_epochs
+        
+        self.buffer = Buffer()
+
+        self.policy = Actor(state_dim, action_dim)
+        self.optimizer = torch.optim.Adam([
+                        {'params': self.policy.actor.parameters(), 'lr': lr_actor},
+                        {'params': self.policy.critic.parameters(), 'lr': lr_critic}
+                    ])
+
+        self.policy_target = Actor(state_dim, action_dim, has_continuous_action_space, action_std_init).to(device)
+        self.policy_target.load_state_dict(self.policy.state_dict())
+
+            
--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Q-learning
 - Q-learning implementation for gym environment ```Mountain Car Discrete```
 - Epsilon Greedy exploration strategy with epsilon decay rate of ```2/episodes``` and floor of epsilon = 0
 - Update Q-table values with:
-$$Q(state,action) = (1 - \alpha)Q(state,action) + \alpha(reward+\gamma argmax_\theta(Q(state,\theta) - Q(state,action)))$$
+$Q(state,action) = (1 - \alpha)Q(state,action) + \alpha(reward+\gamma argmax_\theta(Q(state,\theta) - Q(state,action)))$
 - To handle continuous state environment, organize into buckets for the Q-table to have finite size
 - Validation environment runs every 10 runs taking average of 5 attempts with agent taking only deterministic actions
 -  Training losses:\
@@ -14,17 +14,17 @@ SAC-Discrete
 - SAC discrete implentation for same gym environment ```Mountain Car Discrete```
 - Utilizes double Q-networks for critic, Agent network, and Target networks
 - Update Critic networks with loss function:
-$$
-Loss_C = MSE\_loss(Q1, Q\_target1)+MSE\_loss(Q2,Q\_target2)$$
+$
+Loss_C = MSE\_loss(Q1, Q\_target1)+MSE\_loss(Q2,Q\_target2)$
 - Update Actor network with loss function:
-$$
+$
 Q_1, Q_2 = q\_critic(s)\\
 text{Loss}_a = \sum ( \text{probs} \cdot (\alpha \cdot log\_probs - \min (Q_1,Q2)))
-$$
+$
 - Update target Q-networks with polyak averaging:
-$$
+$
 \theta_{t+1} = \alpha_{t}\theta_{t}\ + (1-\alpha_{t})\theta_{t}
-$$
+$
 
 Comparison with Q-learning
 - Utilize matplotlib script to show comparison between two pickled validation episodes of both Q-learning and SAC-Discrete\
@@ -35,5 +35,7 @@ SAC
 - Implemented Continous version of SAC algorithm based on my Discrete implementation
     - Changed output layer of Policy network to output log standard deviation and mean of a normal distribution. Sampling changed accordingly.
     - Critic networks also accept state and action since actions are now a continuous value.
-- In the validation, curve due to very different reward structure, it goes from 0 to 100 very quickly as there is a small penalty for taking additional moves but a +100 reward for reaching the end which was not in the Discrete model.
+- In the validation, curve due to very different reward structure, it goes from 0 to 100 very quickly as there is a small penalty for taking additional moves but a +100 reward for reaching the end which was not in the Discrete model.\
 ![SAC](images/SAC_eval.png)
+
+PPO
